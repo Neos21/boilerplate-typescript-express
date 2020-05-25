f80bf88a15c94464d2a70f4d3469e13462ffc7ca
Neos21 <neos21@gmail.com>
2020-05-25 21:03:58 +0900

    npm install -D eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+/** ESLint 設定 */
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: [
+  ]
+};
